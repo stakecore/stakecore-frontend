d64c9dac783d972b5bd4c3b2f1777bdb28fab7a6
kuco23 <nseverkar@gmail.com>
2025-12-11 19:25:13 +0100

    chore: remove unused images

--- src/utils/data/testimonials.jsx
+++ src/utils/data/testimonials.jsx
@@ -1,32 +1,32 @@
-import author1 from "../../assets/images/testimonials/author1.jpg"
-import author2 from "../../assets/images/testimonials/author2.jpg"
+//import author1 from "../../assets/images/testimonials/author1.jpg"
+//import author2 from "../../assets/images/testimonials/author2.jpg"
 export const testimonialsData = [
     {
         id: 1,
         name: "Zonathon Doe",
         position: "CEO & Founder X",
-        src: author1,
+        src: '',
         review: "Financial planners help people to knowledge in about how to invest and in save their moneye the most efficient way eve plan ners help people tioniio know ledige in about how."
     },
     {
         id: 2,
         name: "Liana Marie",
         position: "Lead Designer Meta",
-        src: author2,
+        src: '',
         review: "Financial planners help people to knowledge in about how to invest and in save their moneye the most efficient way eve plan ners help people tioniio know ledige in about how."
     },
     {
         id: 3,
         name: "Zonathon Doe",
         position: "CEO & Founder X",
-        src: author1,
+        src: '',
         review: "Financial planners help people to knowledge in about how to invest and in save their moneye the most efficient way eve plan ners help people tioniio know ledige in about how."
     },
     {
         id: 4,
         name: "Liana Marie",
         position: "Lead Designer Meta",
-        src: author2,
+        src: '',
         review: "Financial planners help people to knowledge in about how to invest and in save their moneye the most efficient way eve plan ners help people tioniio know ledige in about how."
     },
 
